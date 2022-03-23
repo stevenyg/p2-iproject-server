d3a98510a2c9b61c9b0f1803a5897b46583d8739
Steven <stevenyang9595@gmail.com>
2022-03-24 00:39:39 +0700

    hot fix deploy

--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,31 @@
-const sendAutoMailer = require('./helpers/nodemailer');
+const nodemailer = require('nodemailer');
 const axios = require('axios');
+
+const sendAutoMailer = (emailReceiver, subject, text) => {
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'stevennodemailer@gmail.com',
+            pass: 'bk1668jn'
+        }
+    });
+
+    var mailOptions = {
+        from: 'stevennodemailer@gmail.com',
+        to: emailReceiver,
+        subject: subject,
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
 
 const scheduler = async (req, res) => {
     try {
